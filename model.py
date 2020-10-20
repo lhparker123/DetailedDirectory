37b2f251a7ab75f0a8f6283cb0ff525938d77ab0
lhparker123 <73168992+lhparker123@users.noreply.github.com>
2020-10-20 12:26:08 +0200

    Update model.py

--- model.py
+++ model.py
@@ -9,6 +9,13 @@ Created on Tue Aug 18 17:52:39 2020
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.utils.testing import ignore_warnings
 from sklearn.exceptions import ConvergenceWarning
+from keras.utils import to_categorical
+from tensorflow.keras import layers
+from tensorflow.keras import activations
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Flatten, Activation, MaxPooling2D, Dropout
 import numpy as np
 import time
 
@@ -35,7 +42,58 @@ class model:
         score = clf.score(self.test, self.tel)
         return clf, score
 
-    def CNN(self):
+    def CNN(self, loadModel, feature):
         """Input CNN code"""
+        print(self.train.shape)
+        
+        N = self.train.shape[0] + self.test.shape[0]
+        
+        train = np.reshape(self.train, (self.train.shape[0], 178, 218, 3))
+        test = np.reshape(self.test, (self.test.shape[0], 178, 218, 3))
+
+        y_train = to_categorical(self.trl, num_classes = 2)
+        y_test = to_categorical(self.tel, num_classes = 2)
+        
+        if loadModel:
+            print('loading model')
+            model = keras.models.load_model('C:/Users/onale/Desktop/Project/'+feature)
+        else:
+            #create model
+            model = Sequential()
+    
+            #convolutional layers
+            model.add(Conv2D(filters = 32, kernel_size=3, strides = 1, padding = 'same', 
+                         input_shape=(178,218,3), activation ='relu'))
+            model.add(MaxPooling2D(pool_size=(2,2)))
+        
+            model.add(Conv2D(filters = 64, kernel_size=3, strides = 1, activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2,2)))
+        
+            model.add(Conv2D(filters = 128, kernel_size=3, strides=1, activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2,2)))
+        
+            model.add(Conv2D(filters = 256, kernel_size=3, strides=1, activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2,2)))
+  
+        
+            #fully connected layers
+            model.add(Flatten())
+            model.add(Dense(units = 64))
+            model.add(Activation('relu'))
+            model.add(Dropout(0.05))
+        
+            model.add(Dense(units = 32))
+            model.add(Activation('relu'))
+            model.add(Dropout(0.05))
+    
+            model.add(Dense(2, activation='softmax'))
+    
+            #compile model using accuracy as a measure of model performance
+            model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    
+        #train model
+        model.fit(train, y_train,validation_data=(test, y_test), epochs=3)
+        model.save('C:/Users/onale/Desktop/Project/'+feature)
+        model.
 
 
